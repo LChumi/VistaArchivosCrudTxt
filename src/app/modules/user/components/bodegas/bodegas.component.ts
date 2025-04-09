90416ed31c6404e8038780bebda3ea8c4de5e8dd
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-04-09 11:39:06 -0500

    fix: cleanup code

--- src/app/modules/user/components/bodegas/bodegas.component.ts
+++ src/app/modules/user/components/bodegas/bodegas.component.ts
@@ -14,7 +14,6 @@ import { Router } from '@angular/router';
 import {faFileExcel, faFileExport, faFilePdf, faWindowClose} from "@fortawesome/free-solid-svg-icons";
 import {ReportsService} from "../../../../core/services/reports.service";
 import {finalize} from "rxjs";
-import {ModelsService} from "../../../../core/services/models.service";
 
 @Component({
   selector: 'app-bodegas',
@@ -28,22 +27,11 @@ export class BodegasComponent implements OnInit {
   id_usuario = sessionStorage.getItem('idUsuario') ?? '';
   id_empresa = sessionStorage.getItem('idEmpresa') ?? '';
   alamcenNarancayImg:string = '../../../../../assets/img/bodega.jpg';
-  movimientosImg:string = '../../../../../assets/img/movimientos.jpg';
   showModal=false
-  modalConta = false
-  isConta = false;
   donwloading=false
-  isSending=false
-
   codigo:string ='0';
   data:string=''
-  emailEmpresarial = '';
-  maxFileSize =1048576; //1MB
 
-  message: string | null = null;
-  selectedFile: File | null = null;
-
-  modelService = inject(ModelsService);
 
 
   constructor(private bodegaService: BodegaService, private dialogService:DialogService,private router: Router, private reportService:ReportsService) { }
@@ -54,51 +42,20 @@ export class BodegasComponent implements OnInit {
       this.logout()
     }
     this.listarBodegas()
-    this.obtenerCorreoEmpresarial()
-
   }
 
   listarBodegas() {
+    console.log(this.id_usuario, "empresa: ", this.id_empresa)
     this.bodegaService.getBodegas(this.id_usuario, this.id_empresa).subscribe(
       (listarBodegas:Bodega[]) => this.listaBodegas=listarBodegas
     )
   }
 
-  obtenerCorreoEmpresarial(){
-    this.modelService.getEmpleado(this.id_usuario).subscribe(
-      empleado => {
-        if (empleado.mailEmpresa){
-          this.emailEmpresarial=empleado.mailEmpresa
-          this.modalConta = true
-          this.isConta = true
-        }
-    }
-    )
-  }
 
   BodegaSelecccionada(bodega: Bodega) {
     this.bodegaSeleccionada = bodega;
     sessionStorage.setItem('bodId', String(this.bodegaSeleccionada.bod_codigo));
     this.dialogService.abrirConfirmacion(bodega.bod_nombre);
-  }
-
-  ingresarMovimientos(nombreBodega: string) {
-    switch (true) {
-      case this.listaBodegas.some(bod => /SUGERIDOS/.test(nombreBodega)):
-        this.router.navigate(['Cumpleaños/observaciones/sugeridos']);
-        break;
-      case this.listaBodegas.some(bod => /BOD. ZHUCAY/.test(nombreBodega) && /BOD. ZHUCAY/.test(bod.bod_nombre)):
-        this.router.navigate(['Cumpleaños/observaciones/mov_pedidos']);
-        break;
-      case this.listaBodegas.some(bod => /PED. ZHUCAY/.test(nombreBodega)):
-        this.router.navigate(['Cumpleaños/observaciones/mov_zhucay']);
-        break;
-      // Agrega más casos según sea necesario para otras bodegas
-      default:
-        alert('No tienes acceso a esta funcion')
-        // Haz algo en caso de que el nombre de la bodega no coincida con ningún caso
-        break;
-    }
   }
 
   logout() {
@@ -144,67 +101,6 @@ export class BodegasComponent implements OnInit {
     } else {
       alert('Por favor, ingrese un número de pedido.');
     }
-  }
-
-  onFileChange(event: any): void {
-    const file = event.target.files[0];
-    if (file) {
-      if (file.size > this.maxFileSize){
-        alert('El archivo no debe exceder 1MB')
-        return;
-      }
-
-      //Verificar tipo de archivo
-      const validExtensions = ['.xml', '.txt', 'csv', '.json'];
-      const fileExtensions = file.name.split('.').pop().toLowerCase();
-      if (!validExtensions.includes('.' + fileExtensions)){
-        alert('Archivo no permitido')
-        return;
-      }
-
-      this.selectedFile = file;
-    }
-  }
-
-  sendString(data: string) {
-    this.isSending= true
-    this.modelService.sendString(data, this.emailEmpresarial).subscribe(
-      success => {
-        this.message = success ? 'Datos subidos satisfactoriamente' : 'Ocurrió un error';
-        this.cleaninputs()
-        this.isSending = false
-      },
-      error => {
-        console.error('Error al enviar el texto ', error)
-        this.message = 'Error al enviar los datos';
-        this.cleaninputs()
-        this.isSending=false
-      }
-    );
-  }
-
-  sendFile() {
-    this.isSending= true
-    if (this.selectedFile && this.emailEmpresarial) {
-      this.modelService.sendFile(this.selectedFile, this.emailEmpresarial).subscribe(
-        success => {
-          this.message = success ? 'Datos subidos satisfactoriamente' : 'Ocurrió un error';
-          this.cleaninputs()
-          this.isSending=false
-        },
-        error => {
-          console.error('Error enviando el archivo:', error);
-          this.message = 'Error al enviar el archivo';
-          this.cleaninputs()
-          this.isSending=false
-        }
-      );
-    }
-  }
-
-  cleaninputs(){
-    this.selectedFile = null;
-    this.data = '';
   }
 
   protected readonly faFileExport = faFileExport;
