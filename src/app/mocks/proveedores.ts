e74a4ae527900809fee633eaba2a1ec528dbad15
LChumi <luischumi.9@gmail.com>
2026-04-16 18:24:38 -0500

    feat:changes into proveedor interface

--- src/app/mocks/proveedores.ts
+++ src/app/mocks/proveedores.ts
@@ -8,49 +8,63 @@
 
 export interface proveedor {
   nombre: string;
+  ruc: string;
 }
 
 export const PROVEEDORES_MOCK: proveedor[] = [
   {
     nombre : "DISTRIBUIDORA COLOMBINA DEL ECUADOR S.A",
-  },
+    ruc : "1792008395001" },
   {
     nombre: "CONFITECA",
+    ruc : "1790084604001" },
+  {
+    nombre: "ALVAREZ CONDO CELSO VICENTE",
+    ruc : "0103784955001" },
+  {
+    nombre: "BERMEO ARGUDO CESAR AUGUSTO",
+    ruc : "0104132220001" },
+  {
+    nombre: "INDUSTRIAL DANEC",
+    ruc : "1790040968001" },
+  {
+    nombre: "IMPORTADORA Y COMERCIALIZADORA PINOS MONCAYO",
+    ruc : "0190128482001" },
+  {
+    nombre: "AUTO&HOME",
+    ruc : "0195144982001" },
+  {
+    nombre: "CHUQUI ORELLANA VICTOR MANUEL",
+    ruc : "0105066971001" },
+  {
+    nombre: "FLORES CORDERO LENIN",
+    ruc : "0101641983001" },
+  {
+    nombre: "MAURAT SUMBA MILTON FRANCISCO",
+    ruc : "0103952941001" },
+  {
+    nombre: "ANTURIOS",
+    ruc : "1791729587001" },
+  {
+    nombre: "RODRIGUEZ SALAZAR EDUARDO XAVIER",
+    ruc : "0103405940001" },
+  {
+    nombre: "SEGARRA CHICA MARIO EDUARDO",
+    ruc : "0101707180001" },
+  {
+    nombre: "JARA MAITA GABRIEL EDUARDO",
+    ruc : "0302884341001"
   },
   {
-    nombre: "ALVAREZ CONDO CELSO VICENTE"
+    nombre: "INDUAL CORAL",
+    ruc : "0302884341001"
   },
   {
-    nombre: "BERMEO ARGUDO CESAR AUGUSTO"
+    nombre: "CONFITES GAMBU",
+    ruc : "1758643488001"
   },
   {
-    nombre: "INDUSTRIAL DANEC"
-  },
-  {
-    nombre: "IMPORTADORA Y COMERCIALIZADORA PINOS MONCAYO"
-  },
-  {
-    nombre: "AUTO%HOME"
-  },
-  {
-    nombre: "CHUQUI ORELLANA VICTOR MANUEL"
-  },
-  {
-    nombre: "FLORES CORDERO LENIN"
-  },
-  {
-    nombre: "MAURAT SUMBA MILTON FRANCISCO"
-  },
-  {
-    nombre: "ANTURIOS"
-  },
-  {
-    nombre: "RODRIGUEZ SALAZAR EDUARDO XAVIER"
-  },
-  {
-    nombre: "SEGARRA CHICA MARIO EDUARDO"
-  },
-  {
-    nombre: "JARA MAITA GABRIEL EDUARDO"
+    nombre: "DULCES GOXUA",
+    ruc : "1793084281001"
   }
 ]
